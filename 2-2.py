1bdc6a41865c7f152aeead09a4294e261f7d3cde
whpark70 <whpark70@gmail.com>
2017-08-15 11:46:22 +0900

    recipe 2.3

--- 2-2.py
+++ 2-2.py
@@ -12,3 +12,14 @@ filtered = [ name for name in filenames if name.endswith(('.c', '.h'))]
 print('filtered: ', filtered)
 
 print(any(name.endswith('.py') for name in filenames))
+
+
+from urllib.request import urlopen
+
+def read_data(name):
+	if name.startswith(('http:','https:', 'ftp:')):
+		return urlopen(name).read()
+	else:
+		with open(name) as f:
+			return f.read()
+
